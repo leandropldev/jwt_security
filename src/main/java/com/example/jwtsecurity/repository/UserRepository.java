735c7ad888197867fb4242aca82ef34be67eadf9
Leandro Reis <developer.reis@gmail.com>
2023-12-25 05:02:52 -0300

    model and repository done

---
+++ src/main/java/com/example/jwtsecurity/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.jwtsecurity.repository;
+
+import com.example.jwtsecurity.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+}
